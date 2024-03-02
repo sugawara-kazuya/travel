d73d08cb80af81435a58d98f996a7a5689e73b0a
sugawara-kazuya <kz515yssg@gmail.com>
2024-03-02 16:57:32 +0900

    ESLint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -246,19 +246,19 @@ export default function Component() {
   );
 }
 
-function LeafIcon(props) {
+function LeafIcon({ width = "24", height = "24", ...rest }) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={width}
+      height={height}
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      {...rest} // 他のすべての受け入れられるプロパティをここに展開
     >
       <path d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z" />
       <path d="M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12" />
@@ -266,19 +266,19 @@ function LeafIcon(props) {
   );
 }
 
-function SearchIcon(props) {
+function SearchIcon({ width = "24", height = "24", ...rest }) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={width}
+      height={height}
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      {...rest} // 他のすべての受け入れられるプロパティをここに展開
     >
       <circle cx="11" cy="11" r="8" />
       <path d="m21 21-4.3-4.3" />
